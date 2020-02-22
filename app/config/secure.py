2348fdda654cc3e8f3d813cf77b2b3a7678f2a17
李翔 <lx70770@gmail.com>
2020-02-22 20:15:41 +0800

    :bug: fix

--- app/config/secure.py
+++ app/config/secure.py
@@ -1,3 +1,4 @@
 SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:Walx00..!@localhost:3306/ginger'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'RuntimeError: The session is unavailable because no secret key was set.  Set the secret_key on the ' \
              'application to something unique and secret. '
